c8fe7bda4b891b0cd97bc19561effa9256e18dd0
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-05-30 00:20:12 +0300

    Add Get Started button and change Greeting text

--- index.js
+++ index.js
@@ -2,13 +2,25 @@
 const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser')
-const Bot = require('messenger-bot')
+const Bot = require('./Bot')
 
 let bot = new Bot({
   token: process.env.PAGE_TOKEN,
   verify: process.env.VERIFY_TOKEN,
   app_secret: process.env.APP_SECRET
 })
+
+bot.setGreetingText('Welcome to EBudgie\'s assistant', (err, data) => {
+  console.log(`Setting GreetingText result ${JSON.stringify(data)}`);
+});
+
+bot.setGetStartedButton([{
+  payload: 'New user connected'
+}], (err, data) => {
+  console.log(`Setting GetStartedButton result ${JSON.stringify(data)}`);
+});
+
+// bot.setPersistentMenu(payload, callback)
 
 bot.on('error', (err) => {
   console.log(err.message)
