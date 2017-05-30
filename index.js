1297c636dd20ceba8893ee399fea88e712b22893
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-05-31 01:16:02 +0300

    Add persistant menu and a sample postback response

--- index.js
+++ index.js
@@ -11,12 +11,55 @@ let bot = new Bot({
 })
 
 bot.setGreetingText('Welcome to EBudgie\'s assistant', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
   console.log(`Setting GreetingText result ${JSON.stringify(data)}`);
+});
+bot.setPersistentMenu([
+  {
+    "title": "Account",
+    "type": "nested",
+    "call_to_actions": [
+      {
+        "title": "Expenses",
+        "type": "postback",
+        "payload": "SHOW_EXPENSES"
+      },
+      {
+        "title": "Incomes",
+        "type": "postback",
+        "payload": "SHOW_INCOMES"
+      },
+      {
+        "title": "Threshold",
+        "type": "postback",
+        "payload": "SHOW_THRESHOLD"
+      }
+    ]
+  },
+  {
+    "type": "web_url",
+    "title": "Visit Website",
+    "url": "https://leon92xx.wixsite.com/ebudgie-website/",
+    "webview_height_ratio": "full"
+  }
+], (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(`Setting setPersistentMenu result ${JSON.stringify(data)}`);
 });
 
 bot.setGetStartedButton([{
   payload: 'New user connected'
 }], (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
   console.log(`Setting GetStartedButton result ${JSON.stringify(data)}`);
 });
 
@@ -33,6 +76,34 @@ bot.on('message', (payload, reply) => {
     if (err) throw err
 
     reply({ text }, (err) => {
+      if (err) throw err
+
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+    })
+  })
+})
+
+bot.on('postback', (payload, reply) => {
+  let text = payload.message.text
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err
+
+    reply({
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "button",
+          "text": "What do you want to do next?",
+          "buttons": [
+            {
+              "type": "account_link",
+              "url": "https://leon92xx.wixsite.com/ebudgie-website/"
+            }
+          ]
+        }
+      }
+    }, (err) => {
       if (err) throw err
 
       console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
