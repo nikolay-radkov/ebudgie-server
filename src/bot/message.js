84c3fabe2c55951749f577cbc336596e77b4ab02
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-06-03 19:44:46 +0300

    Show salary if user is logged and type salary or salaries

--- src/bot/message.js
+++ src/bot/message.js
@@ -1,9 +1,43 @@
+import { isAuthorized, showLoginMenu } from './helpers/auth';
+import { showSalary } from './helpers/salary';
+
 export default (bot) => {
-  return async (payload, reply) => {
-    let text = payload.message.text;
+  return async (data, reply) => {
+    const page_scoped_id = data.sender.id;
+    let text = data.message.text || '';
 
     try {
-      const profile = await bot.getProfile(payload.sender.id);
+      const isLogged = await isAuthorized(page_scoped_id);
+
+      if (!isLogged) {
+        await showLoginMenu(reply);
+      }
+      else {
+        switch (text.toLowerCase()) {
+          case 'expense':
+          case 'expenses':
+
+            break;
+          case 'income':
+          case 'incomes':
+
+            break;
+          case 'threshold':
+          case 'thresholds':
+
+            break;
+          case 'salary':
+          case 'salaries':
+            await showSalary(page_scoped_id, reply);
+            break;
+          default:
+            reply({
+              text: 'Sorry I could not understand that. Please try with different words.'
+            });
+        }
+      }
+
+      const profile = await bot.getProfile(data.sender.id);
       await reply({ text });
       console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`);
     } catch (e) {
