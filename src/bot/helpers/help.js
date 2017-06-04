4ae93f6c54687a97b995ca8c1186f483662a9c0a
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-06-04 20:48:46 +0300

    Make more readable the help message

--- src/bot/helpers/help.js
+++ src/bot/helpers/help.js
@@ -7,32 +7,38 @@ export const showHelp = async (reply) => {
       text: 'Some of the things that you can text me are the following:'
     });
     await reply({
-      text: `menu - open the menu with different options
-expenses - open a menu for expenses 
-incomes - open a menu for incomes 
-threshold - open a menu for threshold`
+      text: `
+'menu' - open the menu with different options
+'expenses' - open a menu for expenses 
+'incomes' - open a menu for incomes 
+'threshold' - open a menu for threshold
+`
     });
     await reply({
       text: `
-salary - show your current salary
-monthly expenses amount - show the amount of your expenses for the current month
-all expenses amount - show the amount of all of your expenses
-monthly expenses - show detailed expenses for the current month
-all expenses - show detailed expenses for all time
-monthly incomes amount - show the amount of your incomes for the current month
-all incomes amount - show the amount of all of your incomes
-monthly incomes - show detailed incomes for the current month
-all incomes - show detailed incomes for all time
+'salary' - show your current salary
+'monthly expenses amount' - show the amount of your expenses for the current month
+'all expenses amount' - show the amount of all of your expenses
+'monthly expenses' - show detailed expenses for the current month
+'all expenses' - show detailed expenses for all time
 `
     });
     await reply({
-      text: `global threshold - show an info for your global threshold
-categories threshold - show an info for your categories threshold
+      text: `
+'monthly incomes amount' - show the amount of your incomes for the current month
+'all incomes amount' - show the amount of all of your incomes
+'monthly incomes' - show detailed incomes for the current month
+'all incomes' - show detailed incomes for all time
 `
     });
-
     await reply({
-      text: 'logout - show a menu for signin out of EBudgie\'s account'
+      text: `
+'global threshold' - show an info for your global threshold
+'categories threshold' - show an info for your categories threshold
+`
+    });
+    await reply({
+      text: '\'logout\' - show a menu for signin out of EBudgie\'s account'
     });
 
   } catch (e) {
