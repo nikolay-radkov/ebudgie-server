284b83209d62499d1a0fc99583cd194624447702
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2017-06-04 13:51:36 +0300

    Add events lib to manipulations over ebudgie's document

---
+++ src/lib/events.js
@@ -0,0 +1,12 @@
+import { filter, sumBy } from 'lodash';
+import moment from 'momemnt';
+
+export const calculateCurrentEvents = (events) => {
+  const filteredEvents = filter(events, (e) => {
+    const eventDate = moment(e.date);
+    const currentDate = moment();
+    return eventDate.month() === currentDate.month() &&
+      eventDate.year() === currentDate.year();
+  });
+  return sumBy(filteredEvents, 'value');
+};
